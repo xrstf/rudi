917920e7f141b6c92756e1433c431e8abe7163e6
xrstf <git@xrstf.de>
2023-11-18 18:30:15 +0100

    fix tests

--- pkg/eval/builtin/util_test.go
+++ pkg/eval/builtin/util_test.go
@@ -34,7 +34,7 @@ func runExpression(t *testing.T, expr string, document any, variables types.Vari
 
 	progContext := eval.NewContext(doc, Functions, variables)
 
-	_, result, err := eval.Run(progContext, program)
+	_, result, err := eval.Run(progContext, &program)
 
 	return result, err
 }
